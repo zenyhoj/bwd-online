4e4e3a677ee461e70b9eafc494a59f10dd797830
zenyhoj <joe.balingit@gmail.com>
2026-06-02 12:00:52 +0800

    fix(tailwind): add missing popover color mapping
    This fixes dropdowns having transparent backgrounds and showing text beneath them.

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,10 @@ const config = {
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))"
         }
       },
       fontFamily: {
